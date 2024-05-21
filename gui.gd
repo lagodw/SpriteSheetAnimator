89f94e8f1203b58d554ce14370f290d07f8ee8e6
Luke G <lagodw@gmail.com>
2024-05-21 16:21:14 -0400

    get input from gui

--- gui.gd
+++ gui.gd
@@ -13,9 +13,16 @@ func generate():
 	var directions = get_directions()
 	var animations = get_animations()
 	var library = AnimationLibrary.new()
-	add_animation(0, 4, 0.1, library)
+	var start_frame = 0
+	for direction in directions:
+		for animation in animations:
+			var dict = animations[animation]
+			add_animation(start_frame, dict['frames'], dict['increment'], 
+				library, animation, direction)
+			start_frame += dict['frames']
 	animPlayer.add_animation_library("animations", library)
-	animPlayer.play("animations/test")
+	print(animPlayer.get_animation_list())
+	animPlayer.play(animPlayer.get_animation_list()[0])
 
 func get_directions() -> Array:
 	var directions = []
@@ -26,14 +33,16 @@ func get_directions() -> Array:
 func get_animations() -> Dictionary:
 	var animations = {}
 	for anim_hbox in $Panel/HBoxContainer/Animations.get_children():
-		if anim_hbox == HBoxContainer:
-			animations['name'] = anim_hbox.get_node("AnimationName").get_text()
-			animations['frames'] = int(anim_hbox.get_node("NumFrames").get_text())
-			animations['increment'] = anim_hbox.get_node("Increment").get_text()
-			animations['looping'] = anim_hbox.get_node("Looping").get_selected()
+		if anim_hbox is HBoxContainer:
+			var anim_name = anim_hbox.get_node("AnimationName").get_text()
+			animations[anim_name] = {} 
+			animations[anim_name]['frames'] = int(anim_hbox.get_node("NumFrames").get_text())
+			animations[anim_name]['increment'] = float(anim_hbox.get_node("Increment").get_text())
+			animations[anim_name]['looping'] = int(anim_hbox.get_node("Looping").get_selected())
 	return(animations)
 
-func add_animation(start_frame: int, num_frames: int, increment: float, library: AnimationLibrary):
+func add_animation(start_frame: int, num_frames: int, increment: float, 
+	library: AnimationLibrary, animation_name: String, direction: String):
 	var anim = Animation.new()
 	for sprite in sprites:
 		var track_index = anim.add_track(Animation.TYPE_VALUE)
@@ -42,7 +51,7 @@ func add_animation(start_frame: int, num_frames: int, increment: float, library:
 		for frame in num_frames:
 			anim.track_insert_key(track_index, track_time, start_frame + frame)
 			track_time += increment
-		anim.loop_mode = Animation.LOOP_LINEAR
-		anim.length = num_frames * increment
-	library.add_animation("test", anim)
+	anim.loop_mode = Animation.LOOP_LINEAR
+	anim.length = num_frames * increment
+	library.add_animation(animation_name + "_" + direction, anim)
 
